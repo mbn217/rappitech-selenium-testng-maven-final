cb510fe84a95871235684c8954b20c2034324164
Mohamed Nheri <nherimohamed@gmail.com>
2024-03-02 16:07:05 -0500

    Updated extent report create test for all test cases

--- src/test/java/com/rappidtech/utilities/ExcelUtils.java
+++ src/test/java/com/rappidtech/utilities/ExcelUtils.java
@@ -19,54 +19,60 @@ public class ExcelUtils {
 
     /**
      * This method will generate First Names and last Names with the help of the faker library
-     * @param sheetName the name of the sheet for your excel
-     * @param fileName the file name of the excel sheet
+     *
+     * @param sheetName       the name of the sheet for your excel
+     * @param fileName        the file name of the excel sheet
+     * @param numberOfRecords how many records we want tp generate
      */
-    public static void generateFirstNameLastNameIntoExcelSheet(String sheetName , String fileName){
+    public static void generateFirstNameLastNameIntoExcelSheet(String sheetName, String fileName, int numberOfRecords) {
 
-        workbook = new XSSFWorkbook();
-        sheet = workbook.createSheet(sheetName);
+        workbook = new XSSFWorkbook(); // initialize workbook object
+        sheet = workbook.createSheet(sheetName); // create a new worksheet inside excel
 
-        for (int i = 0; i < 100 ; i++) {
-            Row row = sheet.createRow(i);
-            Cell firstName = row.createCell(0);
-            Cell lastName = row.createCell(1);
+        for (int i = 0; i < numberOfRecords; i++) {
+            Row row = sheet.createRow(i); // create a new row everytime at index of i -> 0 1 2 3 4 etc...
+            Cell firstName = row.createCell(0); // first column is firstName
+            Cell lastName = row.createCell(1); // second column is lastName
 
-            firstName.setCellValue(faker.name().firstName());
-            lastName.setCellValue(faker.name().lastName());
+            firstName.setCellValue(faker.name().firstName()); // set value of firstName using faker
+            lastName.setCellValue(faker.name().lastName());  // set value of lastName using faker
         }
 
         try {
-            FileOutputStream fileOutputStream = new FileOutputStream(fileName);
-            workbook.write(fileOutputStream);
+            FileOutputStream fileOutputStream = new FileOutputStream(fileName);// create a file
+            workbook.write(fileOutputStream); // write the excel into a file
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }finally {
-            try{
-                workbook.close();
-            }catch(IOException e){
+        } finally {
+            try {
+                workbook.close();// close workbook
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
 
-
-
-
-    public static void generateFirstNameAndPasswordIntoExcelSheet(String sheetName , String fileName, int numberOfRecords){
+    /**
+     * This method will generate First Names and passwords with the help of the faker library
+     *
+     * @param sheetName       the name of the sheet for your excel
+     * @param fileName        the file name of the excel sheet
+     * @param numberOfRecords how many records we want tp generate
+     */
+    public static void generateFirstNameAndPasswordIntoExcelSheet(String sheetName, String fileName, int numberOfRecords) {
 
         workbook = new XSSFWorkbook();
         sheet = workbook.createSheet(sheetName);
 
-        for (int i = 0; i < numberOfRecords ; i++) {
+        for (int i = 0; i < numberOfRecords; i++) {
             Row row = sheet.createRow(i);
             Cell firstName = row.createCell(0);
             Cell password = row.createCell(1);
 
             firstName.setCellValue(faker.name().firstName());
-            password.setCellValue(faker.internet().password());
+            password.setCellValue(faker.internet().password());// set value of password using faker
         }
 
         try {
@@ -76,23 +82,21 @@ public class ExcelUtils {
             throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }finally {
-            try{
+        } finally {
+            try {
                 workbook.close();
-            }catch(IOException e){
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
 
 
-
     /**
-     *
      * @param filePath  the path to the excel file
      * @param sheetName the sheet name insdie your workbook : example "Sheet1"
      */
-    public static void getExcelInstance( String filePath , String sheetName){
+    public static void getExcelInstance(String filePath, String sheetName) {
         try {
             workbook = new XSSFWorkbook(filePath);
             sheet = workbook.getSheet(sheetName);
@@ -102,25 +106,23 @@ public class ExcelUtils {
     }
 
     /**
-     *
      * @return the number of rows present in the Excel sheet
      */
-    public static int getRowCount(){
-        int  rowCount = sheet.getPhysicalNumberOfRows();
+    public static int getRowCount() {
+        int rowCount = sheet.getPhysicalNumberOfRows();
         System.out.println("No of Rows : " + rowCount);
         return rowCount;
     }
 
     /**
-     *
      * @return the number of column present in the Excel sheet
      */
-    public static int getColCount(){
-        int colCount = sheet .getRow(0).getPhysicalNumberOfCells();
+    public static int getColCount() {
+        int colCount = sheet.getRow(0).getPhysicalNumberOfCells();
         return colCount;
     }
 
-    public static String getCellDataString(int rowNum , int colNum){
+    public static String getCellDataString(int rowNum, int colNum) {
         String cellData = sheet.getRow(rowNum).getCell(colNum).getStringCellValue();
         return cellData;
     }
@@ -129,23 +131,23 @@ public class ExcelUtils {
     /**
      * This methold will read excel sheet data set and return them
      * in the form of two dimensional array
+     *
      * @return two dimensional array of the excel dataset
      */
-    public static Object[][] getDataSet(String filePath , String sheetName){
+    public static Object[][] getDataSet(String filePath, String sheetName) {
         getExcelInstance(filePath, sheetName);
         int rowCount = getRowCount();
         int colCount = getColCount();
-        Object [][] data  = new Object[rowCount][colCount];
+        Object[][] data = new Object[rowCount][colCount];
 
-        for(int i = 0 ; i < rowCount ; i++){
-            for(int j = 0 ; j < colCount ; j++){
-                String cellData = getCellDataString(i,j);
+        for (int i = 0; i < rowCount; i++) {
+            for (int j = 0; j < colCount; j++) {
+                String cellData = getCellDataString(i, j);
                 data[i][j] = cellData;
             }
         }
         return data;
     }
-
 
 
 }
